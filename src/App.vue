e3b7d3ec0ba6ee0d1f894e595c9fc9f20af9b3b4
EricaMarzi <erica.marzi.1993@gmail.com>
2024-02-05 17:08:35 +0100

    scaffolding

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 <script>
+import AppMain from './components/AppMain.vue'
 export default {
-
+  name: 'Pokedex',
+  components: {
+    AppMain
+  }
 };
 </script>
 
 <template></template>
 
-<style></style>
+<style lang="scss">
+@use './assets/scss/style.scss'
+</style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,11 @@
+<script>
+import PokemonList from './components/PokemonList'
+export default {
+    name: 'AppMain',
+    components: { PokemonList }
+};
+</script>
+
+<template></template>
+
+<style lang="scss"></style>
